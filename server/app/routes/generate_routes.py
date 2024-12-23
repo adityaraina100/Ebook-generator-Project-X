ca2d3fa2afdfa36a464a55badd31a916a415fa4d
Abhishek Dogra <abhidograaa19@gmail.com>
2024-12-24 01:39:32 +0530

    feat: Added generate book index, chapter content.

--- server/app/routes/generate_routes.py
+++ server/app/routes/generate_routes.py
@@ -1,23 +1,38 @@
 from flask import Blueprint, request, jsonify
-from app.services.ebook_service import generate_content, save_content
-from app.services.ebook_service import generate_chapters
-
-
+from app.services.ebook_service import  save_content, generate_chapters, generateChapterContent
+import json
+import ast
 # Create Blueprint
 generate_bp = Blueprint('generate', __name__)
+
 @generate_bp.route('/generate', methods=['POST'])
 def generate_book():
     data = request.json
-    title=data.get('title')
-    target_audience=data.get('target_audience')
+    title = data.get('title')
+    target_audience = data.get('target_audience')
+    
     if not title or not target_audience:
         return jsonify({"error": "All fields are required"}), 400
 
     try:
-        content = generate_chapters(title,target_audience)
-        print("generate")    
-        save_content(content,"bookIndex.txt")
-        print("saved")    
-        return jsonify({"data": content}),200
+        content = generate_chapters(title, target_audience)
+        save_content(content, "bookIndex.txt")
+
+        with open("saved_books/bookIndex.txt", "r") as file:
+            content = file.read()
+            chapters = ast.literal_eval(content)
+
+        with open("saved_books/finalbook.txt", "r") as file:
+                story = file.read()
+        for chapter in chapters:
+            chapter = chapter.strip() 
+            if not chapter:  # Skip empty lines
+                continue
+            
+            
+            chapter_content = generateChapterContent(chapter,story )
+            save_content(chapter_content, chapter + ".txt")
+        
+        return jsonify({"message": "Book generated successfully!", "link": "/path/to/finalbook.txt"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
